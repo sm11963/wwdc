1254b769d56e0b6f327bfb4549e477d76db833da
Sam Miller <sm11963@gmail.com>
2015-02-11 19:36:08 -0500

    Created initial xcode project.

---
+++ smiller-wwdc/smiller-wwdc/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  smiller-wwdc
+//
+//  Created by Sam Miller on 2/11/15.
+//  Copyright (c) 2015 Sam Miller. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    println("Hello World")
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
